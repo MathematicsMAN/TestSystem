e7e804e0a94aaa56ad38f1bb505d31e089114e9f
Aleev Damir <damir_a_z@mail.ru>
2024-09-09 12:55:47 +0300

    lesson 2

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 from tkinter import *
+from tkinter import messagebox as mb
 
-def dismiss(window):
-    window.grab_release()
-    window.destroy()
 
-def press_yes():
-    window = Toplevel()
-    window.title("Твой ответ")
-    window.geometry("230x50")
-    window.protocol("WM_DELETE_WINDOW", lambda: dismiss(window))  # перехватываем нажатие на крестик
-    close_button = Button(window, text="Не верный", command=lambda: dismiss(window))
-    close_button.pack(anchor="center", expand=1)
-    window.grab_set()
-
-def press_no():
-    window = Toplevel()
-    window.title("Твой ответ")
-    window.geometry("230x50")
-    window.protocol("WM_DELETE_WINDOW", lambda: dismiss(window))  # перехватываем нажатие на крестик
-    close_button = Button(window, text="Верный", command=lambda: dismiss(window))
-    close_button.pack(anchor="center", expand=1)
-    window.grab_set()
+def press_ans():
+    if var.get() == right_ans:
+        mb.showinfo("ответ", "Верно!!")
+    else:
+        mb.showerror("ответ", "Не верно!!")
 
 root = Tk()
 root.title("Тест")
-root.geometry('220x100')
+root.geometry('420x300')
 
-ask1 = Label(root, text="2 + 2 = 3?",
+ask1 = Label(text="Сколько будет 2 + 2?",
              font=('Comic San MS', 24, 'bold'),
              bg='#112233', fg='#00FF00', bd=5
              )
 ask1.pack(anchor='n')
-btnYes = Button(root, text="Да", command=press_yes)
-btnNo = Button(root, text="Нет", command=press_no)
-btnYes.place(x=25, y=60)
-btnNo.place(x=125, y=60)
 
+right_ans = 4
+
+var = IntVar()
+var.set(0)
+ans1 = Radiobutton(text='3', value=3, variable=var,
+                   font=('Comic San MS', 24, 'bold'))
+ans2 = Radiobutton(text='4', value=4, variable=var,
+                   font=('Comic San MS', 24, 'bold'))
+ans3 = Radiobutton(text='5', value=5, variable=var,
+                   font=('Comic San MS', 24, 'bold'))
+ans1.place(x=10, y=60)
+ans2.place(x=10, y=100)
+ans3.place(x=10, y=140)
+
+btn = Button(text="Ответить", command=press_ans)
+btn.place(x=30, y=200)
 
 root.mainloop()
 
